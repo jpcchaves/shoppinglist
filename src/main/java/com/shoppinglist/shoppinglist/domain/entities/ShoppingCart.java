a49eca54cecaefa3a1b8a82ad865a4f49dfaea7d
jpcchaves <jpcchaves@outlook.com>
2023-08-23 13:18:19 -0300

    feat: add shopping cart name and createdAt attributes

--- src/main/java/com/shoppinglist/shoppinglist/domain/entities/ShoppingCart.java
+++ src/main/java/com/shoppinglist/shoppinglist/domain/entities/ShoppingCart.java
@@ -1,16 +1,21 @@
 package com.shoppinglist.shoppinglist.domain.entities;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "shopping_carts")
 public class ShoppingCart {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
 
     @OneToMany(
             fetch = FetchType.EAGER,
@@ -19,13 +24,18 @@ public class ShoppingCart {
     @JoinColumn(name = "shopping_cart_id", referencedColumnName = "id")
     private List<Product> products = new ArrayList<>();
 
+    @CreationTimestamp
+    private Date createdAt;
+
     public ShoppingCart() {
     }
 
     public ShoppingCart(Long id,
-                        List<Product> products) {
+                        String name, List<Product> products, Date createdAt) {
         this.id = id;
+        this.name = name;
         this.products = products;
+        this.createdAt = createdAt;
     }
 
     public Long getId() {
@@ -42,5 +52,21 @@ public class ShoppingCart {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 }
