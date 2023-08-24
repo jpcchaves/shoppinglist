181336502b3ba0b679056153f08a7e49b33aeb43
jpcchaves <jpcchaves@outlook.com>
2023-08-24 14:13:55 -0300

    feat: add list all shopping carts service

--- src/main/java/com/shoppinglist/shoppinglist/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/shoppinglist/shoppinglist/service/impl/ShoppingCartServiceImpl.java
@@ -2,9 +2,13 @@ package com.shoppinglist.shoppinglist.service.impl;
 
 import com.shoppinglist.shoppinglist.domain.entities.ShoppingCart;
 import com.shoppinglist.shoppinglist.payload.dto.ApiMessageResponse;
+import com.shoppinglist.shoppinglist.payload.shoppingcart.ShoppingCartMinDto;
 import com.shoppinglist.shoppinglist.repository.ShoppingCartRepository;
 import com.shoppinglist.shoppinglist.service.usecases.ShoppingCartService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -12,6 +16,25 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     public ShoppingCartServiceImpl(ShoppingCartRepository shoppingCartRepository) {
         this.shoppingCartRepository = shoppingCartRepository;
+    }
+
+    @Override
+    public List<ShoppingCartMinDto> getAll() {
+        List<ShoppingCart> shoppingCarts = shoppingCartRepository.findAll();
+        List<ShoppingCartMinDto> shoppingCartMinDtos = new ArrayList<>();
+
+
+        for (ShoppingCart shoppingCart : shoppingCarts) {
+            shoppingCartMinDtos.add(
+                    new ShoppingCartMinDto(
+                            shoppingCart.getId(),
+                            shoppingCart.getUuid(),
+                            shoppingCart.getName()
+                    )
+            );
+        }
+
+        return shoppingCartMinDtos;
     }
 
     @Override
--- src/main/java/com/shoppinglist/shoppinglist/service/usecases/ShoppingCartService.java
+++ src/main/java/com/shoppinglist/shoppinglist/service/usecases/ShoppingCartService.java
@@ -2,7 +2,12 @@ package com.shoppinglist.shoppinglist.service.usecases;
 
 import com.shoppinglist.shoppinglist.domain.entities.ShoppingCart;
 import com.shoppinglist.shoppinglist.payload.dto.ApiMessageResponse;
+import com.shoppinglist.shoppinglist.payload.shoppingcart.ShoppingCartMinDto;
+
+import java.util.List;
 
 public interface ShoppingCartService {
     ApiMessageResponse create(ShoppingCart request);
+
+    List<ShoppingCartMinDto> getAll();
 }
