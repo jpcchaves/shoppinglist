44123c0c7f5a33290deb169271bb13693455b241
jpcchaves <jpcchaves@outlook.com>
2023-08-24 14:16:20 -0300

    feat: add endpoint to list all shopping carts

--- src/main/java/com/shoppinglist/shoppinglist/controller/ShoppingCartController.java
+++ src/main/java/com/shoppinglist/shoppinglist/controller/ShoppingCartController.java
@@ -2,14 +2,14 @@ package com.shoppinglist.shoppinglist.controller;
 
 import com.shoppinglist.shoppinglist.domain.entities.ShoppingCart;
 import com.shoppinglist.shoppinglist.payload.dto.ApiMessageResponse;
+import com.shoppinglist.shoppinglist.payload.shoppingcart.ShoppingCartMinDto;
 import com.shoppinglist.shoppinglist.service.usecases.ShoppingCartService;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/shopping-carts")
@@ -18,6 +18,11 @@ public class ShoppingCartController {
 
     public ShoppingCartController(ShoppingCartService shoppingCartService) {
         this.shoppingCartService = shoppingCartService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ShoppingCartMinDto>> getAll() {
+        return ResponseEntity.ok(shoppingCartService.getAll());
     }
 
     @PostMapping
