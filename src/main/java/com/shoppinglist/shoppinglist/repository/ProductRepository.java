a92f253c46f45058a364682ad5bb705d9e5d9f72
jpcchaves <jpcchaves@outlook.com>
2023-08-24 13:37:00 -0300

    feat: add jpa method findByIdAndShoppingCart_id

--- src/main/java/com/shoppinglist/shoppinglist/repository/ProductRepository.java
+++ src/main/java/com/shoppinglist/shoppinglist/repository/ProductRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByShoppingCart_Id(Long shoppingCartId);
+
+    Optional<Product> findByIdAndShoppingCart_id(
+            Long productId,
+            Long shoppingCartId);
 }
