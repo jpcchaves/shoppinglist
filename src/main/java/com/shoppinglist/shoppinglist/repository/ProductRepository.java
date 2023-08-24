5dec3060d8f6baf992865375302e98a35a01170d
jpcchaves <jpcchaves@outlook.com>
2023-08-24 13:16:24 -0300

    feat: add findAllByShoppingCart_Id jpa method

--- src/main/java/com/shoppinglist/shoppinglist/repository/ProductRepository.java
+++ src/main/java/com/shoppinglist/shoppinglist/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.shoppinglist.shoppinglist.domain.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+    List<Product> findAllByShoppingCart_Id(Long shoppingCartId);
 }
