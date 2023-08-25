0a6f76e4bd69d1a70dabedcd3337d3dcc8ccd774
João Paulo <jpcchaves@outlook.com>
2023-08-25 12:42:39 -0300

    feat: add usecase to update a product

--- src/main/java/com/shoppinglist/shoppinglist/service/usecases/ProductService.java
+++ src/main/java/com/shoppinglist/shoppinglist/service/usecases/ProductService.java
@@ -4,6 +4,7 @@ import com.itextpdf.text.DocumentException;
 import com.shoppinglist.shoppinglist.domain.entities.Product;
 import com.shoppinglist.shoppinglist.payload.dto.ApiMessageResponse;
 import com.shoppinglist.shoppinglist.payload.product.ProductCreateDto;
+import com.shoppinglist.shoppinglist.payload.product.ProductUpdateDto;
 
 import java.util.List;
 
@@ -11,6 +12,11 @@ public interface ProductService {
     ApiMessageResponse createProduct(ProductCreateDto createProduct);
 
     List<Product> getProducts(Long shoppingCartId);
+
+    ApiMessageResponse updateProduct(
+            Long shoppingCartId,
+            Long id,
+            ProductUpdateDto updateProduct);
 
     byte[] getProductsListPdf(Long shoppingCartId) throws DocumentException;
 
