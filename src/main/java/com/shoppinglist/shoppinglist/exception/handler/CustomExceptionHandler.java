fea1601ca7c6109fb42b8aa0d84a6261f563ed6b
João Paulo <jpcchaves@outlook.com>
2023-08-26 12:12:27 -0300

    feat: add custom exception handler along with methods to handleUnexpectedExceptions and handleResourceNotFoundException

---
+++ src/main/java/com/shoppinglist/shoppinglist/exception/handler/CustomExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.shoppinglist.shoppinglist.exception.handler;
+
+import com.shoppinglist.shoppinglist.exception.ResourceNotFoundException;
+import com.shoppinglist.shoppinglist.exception.model.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+@ControllerAdvice
+@RestController
+public class CustomExceptionHandler {
+    static final Logger logger = Logger.getLogger("Exception");
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleUnexpectedExceptions(
+            Exception ex,
+            WebRequest request) {
+        logger.severe("Error: " + ex.getClass() + " Message: " + ex.getMessage());
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleResourceNotFoundException(
+            ResourceNotFoundException ex,
+            WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
