7c9154040eb45f97dc017bb77f251a6d24483405
jpcchaves <jpcchaves@outlook.com>
2023-08-28 13:17:45 -0300

    refactor: adjusted shopping cart factory to return ShoppingCartListDto

--- src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ConcreteShoppingCartFactory.java
+++ src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ConcreteShoppingCartFactory.java
@@ -1,17 +1,22 @@
 package com.shoppinglist.shoppinglist.factory.shoppingcart;
 
+import com.shoppinglist.shoppinglist.payload.shoppingcart.ShoppingCartListDto;
 import com.shoppinglist.shoppinglist.payload.shoppingcart.ShoppingCartMinDto;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Component
 public class ConcreteShoppingCartFactory implements ShoppingCartFactory {
     @Override
-    public ShoppingCartMinDto createShoppingCart(
+    public ShoppingCartListDto createShoppingCart(
             Long id,
             UUID uuid,
-            String name) {
-        return new ShoppingCartMinDto(id, uuid, name);
+            String name,
+            int productsAmount,
+            Date createdAt) {
+        return new ShoppingCartListDto(id, uuid, name, productsAmount
+                , createdAt);
     }
 }
--- src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ShoppingCartFactory.java
+++ src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ShoppingCartFactory.java
@@ -1,12 +1,16 @@
 package com.shoppinglist.shoppinglist.factory.shoppingcart;
 
+import com.shoppinglist.shoppinglist.payload.shoppingcart.ShoppingCartListDto;
 import com.shoppinglist.shoppinglist.payload.shoppingcart.ShoppingCartMinDto;
 
+import java.util.Date;
 import java.util.UUID;
 
 public interface ShoppingCartFactory {
-    ShoppingCartMinDto createShoppingCart(
+    ShoppingCartListDto createShoppingCart(
             Long id,
             UUID uuid,
-            String name);
+            String name,
+            int productsAmount,
+            Date createdAt);
 }
