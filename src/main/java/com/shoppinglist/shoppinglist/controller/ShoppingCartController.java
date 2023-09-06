ddfef3b440f6475bbfa2cc1e05648925bbc9d408
João Paulo <jpcchaves@outlook.com>
2023-09-05 21:47:21 -0300

    feat: add get shopping cart by id endpoint

--- src/main/java/com/shoppinglist/shoppinglist/controller/ShoppingCartController.java
+++ src/main/java/com/shoppinglist/shoppinglist/controller/ShoppingCartController.java
@@ -1,14 +1,23 @@
 package com.shoppinglist.shoppinglist.controller;
 
-import com.shoppinglist.shoppinglist.payload.dto.ApiMessageResponse;
-import com.shoppinglist.shoppinglist.payload.dto.shoppingcart.ShoppingCartRequestDto;
-import com.shoppinglist.shoppinglist.payload.dto.shoppingcart.ShoppingCartListDto;
-import com.shoppinglist.shoppinglist.service.usecases.ShoppingCartService;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.shoppinglist.shoppinglist.payload.dto.ApiMessageResponse;
+import com.shoppinglist.shoppinglist.payload.dto.shoppingcart.ShoppingCartListDto;
+import com.shoppinglist.shoppinglist.payload.dto.shoppingcart.ShoppingCartRequestDto;
+import com.shoppinglist.shoppinglist.service.usecases.ShoppingCartService;
 
 @RestController
 @RequestMapping("/api/v1/shopping-carts")
@@ -28,6 +37,12 @@ public class ShoppingCartController {
     @PostMapping
     public ResponseEntity<ApiMessageResponse> create(@RequestBody ShoppingCartRequestDto request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(shoppingCartService.create(request));
+    }
+
+    @GetMapping("/{shoppingCartId}")
+    private ResponseEntity<ShoppingCartListDto> getById(
+            @PathVariable(name = "shoppingCartId") Long id) {
+        return ResponseEntity.ok(shoppingCartService.getById(id));
     }
 
     @PutMapping("/{shoppingCartId}")
