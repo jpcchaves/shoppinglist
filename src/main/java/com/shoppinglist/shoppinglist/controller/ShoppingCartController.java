d99d64075ccdc40eab73bdac3fe8963fd4dfd49a
João Paulo <jpcchaves@outlook.com>
2023-09-17 19:57:54 -0300

    feat: add new request param to filter shopping carts by name

--- src/main/java/com/shoppinglist/shoppinglist/controller/ShoppingCartController.java
+++ src/main/java/com/shoppinglist/shoppinglist/controller/ShoppingCartController.java
@@ -3,17 +3,10 @@ package com.shoppinglist.shoppinglist.controller;
 import java.util.List;
 
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.shoppinglist.shoppinglist.payload.dto.ApiMessageResponse;
 import com.shoppinglist.shoppinglist.payload.dto.shoppingcart.ShoppingCartListDto;
@@ -30,9 +23,11 @@ public class ShoppingCartController {
         this.shoppingCartService = shoppingCartService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<ShoppingCartListDto>> getAll() {
-        return ResponseEntity.ok(shoppingCartService.getAll());
+    @GetMapping()
+    public ResponseEntity<List<ShoppingCartListDto>> getAll(
+            @RequestParam(name = "name", required = false) String name
+    ) {
+        return ResponseEntity.ok(shoppingCartService.getAll(name));
     }
 
     @PostMapping
