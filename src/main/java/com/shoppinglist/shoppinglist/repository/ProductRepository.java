a8b5ae1fae6f13197e6a28a037dff9e1c8d43f7b
João Paulo <jpcchaves@outlook.com>
2023-09-19 22:02:47 -0300

    feat: add jpa method to find product by name

--- src/main/java/com/shoppinglist/shoppinglist/repository/ProductRepository.java
+++ src/main/java/com/shoppinglist/shoppinglist/repository/ProductRepository.java
@@ -14,4 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByIdAndShoppingCart_id(
             Long productId,
             Long shoppingCartId);
+
+    List<Product> findByNameContainingIgnoreCase(String name);
 }
