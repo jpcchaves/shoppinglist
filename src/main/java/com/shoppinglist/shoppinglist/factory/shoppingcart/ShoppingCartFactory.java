86b1db8ac1c092b734c6f883071b63436de66a4e
João Paulo <jpcchaves@outlook.com>
2023-09-29 22:58:50 -0300

    feat: add new usecase in shopping cart factory

--- src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ShoppingCartFactory.java
+++ src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ShoppingCartFactory.java
@@ -1,8 +1,10 @@
 package com.shoppinglist.shoppinglist.factory.shoppingcart;
 
+import com.shoppinglist.shoppinglist.payload.dto.product.ProductDto;
 import com.shoppinglist.shoppinglist.payload.dto.shoppingcart.ShoppingCartListDto;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface ShoppingCartFactory {
@@ -12,5 +14,13 @@ public interface ShoppingCartFactory {
             String name,
             String description,
             int productsAmount,
+            Date createdAt);
+
+    ShoppingCartListDto createShoppingCartWithTotalPrice(
+            Long id,
+            UUID uuid,
+            String name,
+            String description,
+            List<ProductDto> productList,
             Date createdAt);
 }
