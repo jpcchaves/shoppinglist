1d3f90570d97e00db2ae8811b468f49fce9a6743
João Paulo <jpcchaves@outlook.com>
2023-09-29 22:59:21 -0300

    feat: implemented new usecase in concrete shopping cart factory method createShoppingCartWithTotalPrice

--- src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ConcreteShoppingCartFactory.java
+++ src/main/java/com/shoppinglist/shoppinglist/factory/shoppingcart/ConcreteShoppingCartFactory.java
@@ -1,9 +1,13 @@
 package com.shoppinglist.shoppinglist.factory.shoppingcart;
 
+import com.shoppinglist.shoppinglist.domain.entities.Product;
+import com.shoppinglist.shoppinglist.payload.dto.product.ProductDto;
 import com.shoppinglist.shoppinglist.payload.dto.shoppingcart.ShoppingCartListDto;
+import com.shoppinglist.shoppinglist.utils.product.ProductUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -18,5 +22,18 @@ public class ConcreteShoppingCartFactory implements ShoppingCartFactory {
             Date createdAt) {
         return new ShoppingCartListDto(id, uuid, name, description, productsAmount
                 , createdAt);
+    }
+
+    @Override
+    public ShoppingCartListDto createShoppingCartWithTotalPrice(
+            Long id,
+            UUID uuid,
+            String name,
+            String description,
+            List<ProductDto> productList,
+            Date createdAt) {
+        return new ShoppingCartListDto(id, uuid, name, description, productList.size()
+                , ProductUtils.calculateShoppingCartTotalPrice(productList), createdAt);
+
     }
 }
